5a7d2513fe07ece06c35245fbd191bf5a58c06e7
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-06-15 12:42:49 +0200

    Default DEBUG to False

--- swapi/settings.py
+++ swapi/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get(
     'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
 )
 
-DEBUG = bool(os.environ.get('DEBUG', True))
+DEBUG = bool(os.environ.get('DEBUG', False))
 print "DEBUG: "
 print DEBUG
 # Because test settings will trigger KEEN.io hits
